5d5b6b1b8756405c8452cd7242cf1f4eab8c75b1
Nopacketloss <antonioneto_08@hotmail.com>
2024-05-05 08:42:20 -0300

    Organiando os diretórios do projeto

---
+++ Java/java-anatomia-classes/src/dio/Antonio/primeira semana/BoletimEstudante.java
@@ -0,0 +1,18 @@
+public class BoletimEstudante {
+
+    public class BoletimEstudantil {
+
+        public static void main(String[] args) {
+        int mediaFinal = 6;
+        if(mediaFinal<6)	
+        System.out.println("REPROVADO"); 
+        else if(mediaFinal==6)
+        System.out.println("PROVA MINERVA"); 
+        else
+        System.out.println("APROVADO"); 		
+       
+      }
+        
+    }
+    
+}
