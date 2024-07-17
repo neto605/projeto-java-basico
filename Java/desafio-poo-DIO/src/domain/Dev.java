67c885d569efbd4a6a3128cf0c4a56cdcb9c8766
Neto605 <antonioneto_08@hotmail.com>
2024-07-17 13:42:19 -0400

    Commit de desafio

--- Java/desafio-poo-DIO/src/domain/Dev.java
+++ Java/desafio-poo-DIO/src/domain/Dev.java
@@ -2,6 +2,7 @@ package domain;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -11,13 +12,33 @@ public class Dev {
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
     public void inscreverBootcamp(Bootcamp bootcamp) {   
-       this.conteudosInscritos.add(bootcamp.getConteudos());
+       this.conteudosInscritos.addAll(bootcamp.getConteudos());
+       bootcamp.getDevsIncritos().add(this);
 
     }
 
-    public void progredir() {}
+    public void progredir() {   
+       Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
 
-    public void calcularTotalXP() {}
+       if (conteudo.isPresent()){
+        this.conteudosConcluidos.add(conteudo.get());
+        this.conteudosInscritos.remove(conteudo.get());
+
+       } else {
+
+        System.err.println(" Tu não tá matriculado em nenhum conteúdo!");
+       }
+
+    }
+
+    public double calcularTotalXP() {
+        
+       return this.conteudosConcluidos
+        .stream()
+        .mapToDouble(Conteudo :: calcularXP)
+        .sum();
+
+    }
 
     // Getters
     public String getNome() {
--- Java/desafio-poo-DIO/src/domain/Main.java
+++ Java/desafio-poo-DIO/src/domain/Main.java
@@ -16,8 +16,8 @@ public class Main {
         curso2.setDescricao("Aumenta o poder de ataque em 30% por 60 segundos.");
         curso2.setCargaHoraria(8);
 
-        System.out.println(curso1);
-        System.out.println(curso2);
+       // System.out.println(curso1);
+        //System.out.println(curso2);
 
         Mentorias mentoria = new Mentorias();
 
@@ -25,7 +25,47 @@ public class Main {
         mentoria.setDescricao("Isso é uma mentoria");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(mentoria);
+        //System.out.println(mentoria);
+
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos incritos Camila" + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        devCamila.progredir();
+        System.out.println("Conteudos incritos Camila" + devCamila.getConteudosConcluidos());
+        System.out.println("Conteudos incritos Camila" + devCamila.getConteudosInscritos());
+        System.out.println("XP" + devCamila.calcularTotalXP());
+
+        System.out.println("------");
+
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos incritos João" + devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        System.out.println("Conteudos incritos Camila" + devCamila.getConteudosConcluidos());
+        System.out.println("Conteudos incritos João" + devJoao.getConteudosInscritos());
+        System.out.println("XP" + devJoao.calcularTotalXP());
+
+
+
+
+
+        
+
+
+
+
 
     
 
